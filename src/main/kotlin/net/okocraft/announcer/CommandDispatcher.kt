6bc7e7a7034dda888e6977820297fd750c6b2ea8
akaregi <akg.tachibana@gmail.com>
2019-07-30 00:25:50 +0900

    著作権表示

--- src/main/kotlin/net/okocraft/announcer/CommandDispatcher.kt
+++ src/main/kotlin/net/okocraft/announcer/CommandDispatcher.kt
@@ -1,3 +1,21 @@
+/*
+ * This is a part of Announcer.
+ * Copyright (C) 2019 AKANE AKAGI (akaregi)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.okocraft.announcer
 
 import org.bukkit.ChatColor
@@ -5,6 +23,9 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 
+/**
+ * @author AKANE AKAGI (akaregi)
+ */
 class CommandDispatcher(private val plugin: Announcer) : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         val config = plugin.configuration
