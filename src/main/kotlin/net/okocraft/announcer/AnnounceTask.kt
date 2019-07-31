cf7fa64c8697a55d5cc0f473952126c9758a203f
akaregi <akg.tachibana@gmail.com>
2019-07-31 15:21:46 +0900

    .also で処理を簡約

--- src/main/kotlin/net/okocraft/announcer/AnnounceTask.kt
+++ src/main/kotlin/net/okocraft/announcer/AnnounceTask.kt
@@ -27,12 +27,10 @@ class AnnounceTask(private val config: Config): Runnable {
     override fun run() {
         val prefix = config.prefix
         val messages = config.messages
-        val message = messages.pop()
+        val message = messages.pop().also { messages.addLast(it) }
 
         Bukkit.getOnlinePlayers().forEach { player ->
             message.split("\n").forEach { m -> player.sendMessage(prefix + m) }
         }
-
-        messages.addLast(message)
     }
 }
